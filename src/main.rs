338771a31657caed3a9fd98acb3844a056b177ef
Joey Yandle <xoloki@gmail.com>
2022-11-05 18:08:10 -0400

    import bindings in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+#[allow(unused_variables)]
+#[allow(dead_code)]
+#[allow(non_upper_case_globals)]
+mod bindings;
+
+use crate::bindings::secp256k1_scalar_one;
+
 fn main() {
+    unsafe {
+        let scalar = secp256k1_scalar_one;
+    }
     println!("Hello, world!");
 }
