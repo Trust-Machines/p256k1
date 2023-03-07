34408e9e5fad8926e5e3476b20b565ed12554282
Jacinta Ferrant <jferrant@Jacintas-MacBook-Pro.local>
2023-03-07 13:11:51 -0800

    Pass context everywhere inside ecdsa.rs

--- src/ecdsa.rs
+++ src/ecdsa.rs
@@ -28,23 +28,16 @@ PubKey is a wrapper around libsecp256k1's secp256k1_pubkey struct.
 pub struct PubKey {
     /// The wrapped secp256k1_pubkey public key
     key: secp256k1_pubkey,
-    /// The context associated with the public key
-    context: Context,
 }
 
 impl PubKey {
     /// Construct a public key from a given secret key
-    pub fn new(sec_key: &Scalar) -> Result<Self, Error> {
+    pub fn new(ctx: &Context, sec_key: &Scalar) -> Result<Self, Error> {
         let mut pub_key = Self {
             key: secp256k1_pubkey { data: [0; 64] },
-            context: Context::default(),
         };
         if unsafe {
-            secp256k1_ec_pubkey_create(
-                pub_key.context.context,
-                &mut pub_key.key,
-                sec_key.to_bytes().as_ptr(),
-            )
+            secp256k1_ec_pubkey_create(ctx.context, &mut pub_key.key, sec_key.to_bytes().as_ptr())
         } == 0
         {
             return Err(Error::InvalidSecretKey);
@@ -59,20 +52,17 @@ Signature is a wrapper around libsecp256k1's secp256k1_ecdsa_signature struct.
 pub struct Signature {
     /// The wrapped libsecp256k1 signature
     pub signature: secp256k1_ecdsa_signature,
-    /// The context associated with the given signature
-    pub context: Context,
 }
 
 impl Signature {
     /// Construct an ECDSA signature
-    pub fn new(hash: &[u8], sec_key: &Scalar) -> Result<Self, Error> {
+    pub fn new(ctx: &Context, hash: &[u8], sec_key: &Scalar) -> Result<Self, Error> {
         let mut sig = Self {
             signature: secp256k1_ecdsa_signature { data: [0; 64] },
-            context: Context::default(),
         };
         if unsafe {
             secp256k1_ecdsa_sign(
-                sig.context.context,
+                ctx.context,
                 &mut sig.signature,
                 hash.as_ptr(),
                 sec_key.to_bytes().as_ptr(),
@@ -86,49 +76,22 @@ impl Signature {
         Ok(sig)
     }
 
-    /// Verify an ECDSA signature
-    pub fn verify(&self, hash: &[u8], pub_key: &PubKey) -> bool {
-        1 == unsafe {
-            secp256k1_ecdsa_verify(
-                self.context.context,
-                &self.signature,
-                hash.as_ptr(),
-                &pub_key.key,
-            )
-        }
+    /// Create an ECDSA signature given a slice of signed data.
+    /// Note it also serializes the data in compact (64 byte) format
+    pub fn try_from_slice(ctx: &Context, data: &[u8]) -> Result<Self, Error> {
+        let data: [u8; 64] = data[0..].try_into()?;
+        Signature::try_from_array(ctx, data)
     }
 
-    /// Returns the signature's deserialized underlying data
-    pub fn to_bytes(&self) -> [u8; 64] {
-        let mut bytes = [0u8; 64];
-        //Deserialize the signature's data
-        unsafe {
-            secp256k1_ecdsa_signature_serialize_compact(
-                self.context.context,
-                bytes.as_mut_ptr(),
-                &self.signature,
-            );
-        }
-        bytes
-    }
-}
-
-impl TryFrom<[u8; 64]> for Signature {
-    type Error = Error;
     /// Create an ECDSA signature given an array of signed data.
     /// Note it also serializes the data in compact (64 byte) format
-    fn try_from(data: [u8; 64]) -> Result<Self, Self::Error> {
+    pub fn try_from_array(ctx: &Context, data: [u8; 64]) -> Result<Self, Error> {
         let mut sig = Self {
             signature: secp256k1_ecdsa_signature { data: [0u8; 64] },
-            context: Context::default(),
         };
         //Attempt to serialize the data into the signature
         let parsed = unsafe {
-            secp256k1_ecdsa_signature_parse_compact(
-                sig.context.context,
-                &mut sig.signature,
-                data.as_ptr(),
-            )
+            secp256k1_ecdsa_signature_parse_compact(ctx.context, &mut sig.signature, data.as_ptr())
         };
         if parsed == 0 {
             return Err(Error::TryFrom(format!(
@@ -137,15 +100,26 @@ impl TryFrom<[u8; 64]> for Signature {
         }
         Ok(sig)
     }
-}
 
-impl TryFrom<&[u8]> for Signature {
-    type Error = Error;
-    /// Create an ECDSA signature given a slice of signed data.
-    /// Note it also serializes the data in compact (64 byte) format
-    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
-        let data: [u8; 64] = data[0..].try_into()?;
-        Ok(Signature::try_from(data)?)
+    /// Verify an ECDSA signature
+    pub fn verify(&self, ctx: &Context, hash: &[u8], pub_key: &PubKey) -> bool {
+        1 == unsafe {
+            secp256k1_ecdsa_verify(ctx.context, &self.signature, hash.as_ptr(), &pub_key.key)
+        }
+    }
+
+    /// Returns the signature's deserialized underlying data
+    pub fn to_bytes(&self, ctx: &Context) -> [u8; 64] {
+        let mut bytes = [0u8; 64];
+        //Deserialize the signature's data
+        unsafe {
+            secp256k1_ecdsa_signature_serialize_compact(
+                ctx.context,
+                bytes.as_mut_ptr(),
+                &self.signature,
+            );
+        }
+        bytes
     }
 }
 
@@ -157,10 +131,13 @@ mod tests {
 
     #[test]
     fn signature_generation() {
+        //Create a context
+        let ctx = Context::default();
+
         // Generate a secret and public key
         let mut rnd = OsRng::default();
         let sec_key = Scalar::random(&mut rnd);
-        let pub_key = PubKey::new(&sec_key).unwrap();
+        let pub_key = PubKey::new(&ctx, &sec_key).unwrap();
 
         // Instead of signing a message directly, must sign a 32-byte hash of it.
         let msg = b"Hello, world!";
@@ -168,47 +145,59 @@ mod tests {
         hasher.update(msg);
         let msg_hash = hasher.finalize();
         // Generate a ECDSA signature
-        let sig = Signature::new(&msg_hash, &sec_key).unwrap();
+        let sig = Signature::new(&ctx, &msg_hash, &sec_key).unwrap();
 
         // Verify the generated signature is valid using the msg_hash and corresponding public key
-        assert!(sig.verify(&msg_hash, &pub_key));
+        assert!(sig.verify(&ctx, &msg_hash, &pub_key));
     }
 
     #[test]
     fn from() {
+        //Create a context
+        let ctx = Context::default();
+
+        // Create random data bytes to serialize
         let mut rng = OsRng::default();
         let mut bytes = [0u8; 64];
-
         rng.fill_bytes(&mut bytes);
 
         let sig_from_struct = Signature {
             signature: secp256k1_ecdsa_signature { data: bytes },
-            context: Context::default(),
         };
-        let sig_from_slice = Signature::try_from(bytes.as_slice()).unwrap();
-        let sig_from_array = Signature::try_from(bytes).unwrap();
+        let sig_from_slice = Signature::try_from_slice(&ctx, bytes.as_slice()).unwrap();
+        let sig_from_array = Signature::try_from_array(&ctx, bytes).unwrap();
 
-        assert_ne!(sig_from_struct.to_bytes(), sig_from_slice.to_bytes());
-        assert_ne!(sig_from_struct.to_bytes(), sig_from_array.to_bytes());
-        assert_eq!(sig_from_array.to_bytes(), sig_from_slice.to_bytes());
+        assert_ne!(
+            sig_from_struct.to_bytes(&ctx),
+            sig_from_slice.to_bytes(&ctx)
+        );
+        assert_ne!(
+            sig_from_struct.to_bytes(&ctx),
+            sig_from_array.to_bytes(&ctx)
+        );
+        assert_eq!(sig_from_array.to_bytes(&ctx), sig_from_slice.to_bytes(&ctx));
 
         let mut too_small = [0u8; 63];
         rng.fill_bytes(&mut too_small);
-        assert!(Signature::try_from(too_small.as_slice()).is_err());
+        assert!(Signature::try_from_slice(&ctx, too_small.as_slice()).is_err());
 
         let mut too_big = [0u8; 65];
         rng.fill_bytes(&mut too_big);
-        assert!(Signature::try_from(too_big.as_slice()).is_err());
+        assert!(Signature::try_from_slice(&ctx, too_big.as_slice()).is_err());
     }
 
     #[test]
     fn serialize_deserialize() {
+        //Create a context
+        let ctx = Context::default();
+
+        // Generate random data bytes
         let mut rng = OsRng::default();
         let mut bytes = [0u8; 64];
         rng.fill_bytes(&mut bytes);
 
-        //Serialize with try_from and deserialize with to_bytes
-        let sig = Signature::try_from(bytes).unwrap();
-        assert_eq!(sig.to_bytes(), bytes);
+        //Serialize with try_from_array and deserialize with to_bytes
+        let sig = Signature::try_from_array(&ctx, bytes).unwrap();
+        assert_eq!(sig.to_bytes(&ctx), bytes);
     }
 }
