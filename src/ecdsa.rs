cd1f7e093e33b53a0c28367e9b41203e881a2b9f
Jacinta Ferrant <jferrant@Jacintas-MacBook-Pro.local>
2023-03-06 16:07:36 -0800

    Add signature generation test

--- src/ecdsa.rs
+++ src/ecdsa.rs
@@ -8,8 +8,6 @@ use crate::scalar::Scalar;
 #[derive(Debug)]
 /// Errors in ECDSA signature operations
 pub enum Error {
-    /// Error occurred signing ECDSA Signature
-    SigningFailed,
     /// Error occurred due to invalid secret key
     InvalidSecretKey,
 }
@@ -42,37 +40,66 @@ Signature is a wrapper around libsecp256k1's internal secp256k1_ecdsa_signature 
 */
 pub struct Signature {
     /// The wrapped libsecp256k1 signature
-    pub sig: secp256k1_ecdsa_signature,
+    pub signature: secp256k1_ecdsa_signature,
 }
 
 impl Signature {
     /// Construct an ECDSA signature
-    pub fn new(ctx: &Context, secret_key: &Scalar, hash: &[u8]) -> Result<Self, Error> {
-        let mut signature = Self {
-            sig: secp256k1_ecdsa_signature { data: [0; 64] },
+    pub fn new(ctx: &Context, hash: &[u8], sec_key: &Scalar) -> Result<Self, Error> {
+        let mut sig = Self {
+            signature: secp256k1_ecdsa_signature { data: [0; 64] },
         };
-        let null = std::ptr::null_mut::<::std::os::raw::c_void>();
-
-        let res = unsafe {
+        if unsafe {
             secp256k1_ecdsa_sign(
                 ctx.context,
-                &mut signature.sig,
+                &mut sig.signature,
                 hash.as_ptr(),
-                secret_key.to_bytes().as_ptr(),
+                sec_key.to_bytes().as_ptr(),
                 None,
-                null,
+                std::ptr::null_mut::<::std::os::raw::c_void>(),
             )
-        };
-        if res == 0 {
-            return Err(Error::SigningFailed);
+        } == 0
+        {
+            return Err(Error::InvalidSecretKey);
         }
-        Ok(signature)
+        Ok(sig)
     }
 
     /// Verify an ECDSA signature
-    pub fn verify(self, ctx: &Context, pub_key: &PubKey, hash: &[u8]) -> bool {
-        let valid =
-            unsafe { secp256k1_ecdsa_verify(ctx.context, &self.sig, hash.as_ptr(), &pub_key.key) };
+    pub fn verify(self, ctx: &Context, hash: &[u8], pub_key: &PubKey) -> bool {
+        let valid = unsafe {
+            secp256k1_ecdsa_verify(ctx.context, &self.signature, hash.as_ptr(), &pub_key.key)
+        };
         valid == 1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand_core::OsRng;
+
+    #[test]
+    fn signature_generation() {
+        let ctx = Context::default();
+
+        // Generate a secret and public key
+        let mut rnd = OsRng::default();
+        let sec_key = Scalar::random(&mut rnd);
+        let pub_key = PubKey::new(&ctx, &sec_key).unwrap();
+
+        // Instead of signing a message directly, must sign a 32-byte hash of it.
+        // Here the message is "Hello, world!" hashed usings SHA-256.
+        let msg_hash: [u8; 32] = [
+            0x31, 0x5F, 0x5B, 0xDB, 0x76, 0xD0, 0x78, 0xC4, 0x3B, 0x8A, 0xC0, 0x06, 0x4E, 0x4A,
+            0x01, 0x64, 0x61, 0x2B, 0x1F, 0xCE, 0x77, 0xC8, 0x69, 0x34, 0x5B, 0xFC, 0x94, 0xC7,
+            0x58, 0x94, 0xED, 0xD3,
+        ];
+
+        // Generate a ECDSA signature
+        let sig = Signature::new(&ctx, &msg_hash, &sec_key).unwrap();
+
+        // Verify the generated signature is valid using the msg_hash and corresponding public key
+        assert!(sig.verify(&ctx, &msg_hash, &pub_key));
     }
 }
